823b30a081e1665fef6dae1a7ceb71bc589e9527
Natnael Tekletsadik <isaacnatnael@gmail.com>
2024-12-17 10:48:37 -0500

    add styling to calendar component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalendarComponent } from './calendar/calendar.component';
+import { CanbanComponent } from './canban/canban.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'calendar', component: CalendarComponent},
+  {path: 'canban', component: CanbanComponent},
+  {path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, SimpleChange, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'prodApp-calendar',
   standalone: false,
-  
+
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.css'
 })
 export class CalendarComponent {
+  dayName: string[] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+  days: number[] = Array.from({ length: 35 }, (_, i) => i + 1);
+
+  constructor() {}
 
 }
