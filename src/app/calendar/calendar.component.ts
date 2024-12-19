dd2f66c2c79d1535b8b5936e5d3cae7cd6edea3c
Natnael Tekletsadik <isaacnatnael@gmail.com>
2024-12-19 16:37:54 -0500

    fill calendar with data and add styling

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,5 +1,12 @@
 import { Component, SimpleChange, SimpleChanges } from '@angular/core';
 
+interface DataObject {
+    year: number,
+    month: number,
+    day: number,
+    activity: string
+}
+
 @Component({
   selector: 'prodApp-calendar',
   standalone: false,
@@ -11,6 +18,56 @@ export class CalendarComponent {
   days!: number[] ;
   month: number = this.currentDate.getMonth();
   year: number = this.currentDate.getFullYear();
+  data: DataObject[] = [
+    {
+      year: 2025,
+      month: 1,
+      day: 23,
+      activity: "gym",
+    },
+    {
+      year: 2025,
+      month: 1,
+      day: 1,
+      activity: "gym"
+    },
+    {
+      year: 2025,
+      month: 1,
+      day: 2,
+      activity: "gym",
+    },
+    {
+      year: 2025,
+      month: 1,
+      day: 13,
+      activity: "gym"
+    },
+    {
+      year: 2025,
+      month: 1,
+      day: 30,
+      activity: "gym",
+    },
+    {
+      year: 2025,
+      month: 1,
+      day: 10,
+      activity: "gym"
+    },
+    {
+      year: 2025,
+      month: 1,
+      day: 16,
+      activity: "gym",
+    },
+    {
+      year: 2025,
+      month: 1,
+      day: 28,
+      activity: "gym"
+    },
+  ];
 
   constructor() {
     this.days = this.fillDays();
@@ -47,7 +104,7 @@ export class CalendarComponent {
           return 0;
         }
       }
-    )
+    );
   }
 
   toNextMonth () {
@@ -66,4 +123,14 @@ export class CalendarComponent {
     console.log(this.currentDate);
   }
 
+  getDataOfDay(day: number) {
+    let year: number = this.year;
+    let monthString: string = this.getMonthName(this.month);
+
+    // console.log(`monthString ${monthString} - type ${typeof(monthString)}`)
+    let dayData = this.data?.find( val => val.year === this.year && val.month === this.month && val.day === day )
+    console.log(dayData);
+    return dayData?.activity || "";
+
+  }
 }
